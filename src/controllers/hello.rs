d493984936c639f5c5e0b656cd2771d6b502816f
Androse <braithanderson1@gmail.com>
2023-10-16 23:41:04 +1000

    add hello controller

---
+++ src/controllers/hello.rs
@@ -0,0 +1,35 @@
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::{IntoResponse, Json},
+    routing::get,
+    Router,
+};
+use serde::Serialize;
+use utoipa::ToSchema;
+
+#[derive(Serialize, ToSchema)]
+pub struct Hello {
+    message: String,
+}
+
+pub fn hello_routes() -> Router {
+    Router::new().route("/hello/:name", get(handler))
+}
+
+#[utoipa::path(
+    get,
+    request_body = HelloBody,
+    path = "/hello/{name}",
+    responses(
+        (status = 200, body = Hello)
+    )
+)]
+pub async fn handler(Path(name): Path<String>) -> impl IntoResponse {
+    (
+        StatusCode::OK,
+        Json(Hello {
+            message: format!("Hello, {}!", name),
+        }),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+use axum::Router;
 use std::net::SocketAddr;
-
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Json},
-    routing::get,
-    Router,
-};
-use serde::Serialize;
 use tower_http::trace::{self, TraceLayer};
 use tracing::Level;
-use utoipa::{OpenApi, ToSchema};
+use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
+
+mod controllers;
+
+use crate::controllers::hello::hello_routes;
+
+use self::controllers::hello::Hello;
+
+#[derive(OpenApi)]
+#[openapi(paths(controllers::hello::handler), components(schemas(Hello)))]
+struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
@@ -20,19 +23,14 @@ async fn main() {
         .compact()
         .init();
 
-    #[derive(OpenApi)]
-    #[openapi(paths(handler), components(schemas(Hello)))]
-    struct ApiDoc;
-
     let app = Router::new()
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-        .route("/", get(handler))
-        .route("/hello/:name", get(handler))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
                 .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-        );
+        )
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .merge(hello_routes());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -42,25 +40,4 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-#[derive(Serialize, ToSchema)]
-struct Hello {
-    message: String,
-}
-
-#[utoipa::path(
-       get,
-       path = "/hello/:name",
-       responses(
-              (status = 200, body = Hello)
-       )
-)]
-async fn handler() -> impl IntoResponse {
-    (
-        StatusCode::OK,
-        Json(Hello {
-            message: "Hello, World!".to_owned(),
-        }),
-    )
 }
