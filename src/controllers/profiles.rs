35e9d501f5eaeb19f4791ddc3c8fdeb5c489ccf6
Androse <braithanderson1@gmail.com>
2023-10-29 21:21:28 +1000

    break into seperate methods

--- src/controllers/profiles.rs
+++ src/controllers/profiles.rs
@@ -6,7 +6,7 @@ use axum::{
     Router,
 };
 use diesel::prelude::*;
-use diesel_async::RunQueryDsl;
+use diesel_async::{AsyncPgConnection, RunQueryDsl};
 use uuid::Uuid;
 
 use crate::{
@@ -28,34 +28,41 @@ pub fn profile_routes(db_pool: Pool) -> Router {
         .add_logging()
 }
 
+async fn fetch_profile_by_id(conn: &mut AsyncPgConnection, id: Uuid) -> QueryResult<Profile> {
+    profiles::table
+        .filter(profiles::id.eq(id))
+        .first::<Profile>(conn)
+        .await
+}
+
+async fn fetch_user_disciplines_by_profile_id(
+    conn: &mut AsyncPgConnection,
+    profile_id: Uuid,
+) -> QueryResult<Vec<Discipline>> {
+    profiles::table
+        .inner_join(user_disciplines::table)
+        .filter(profiles::id.eq(profile_id))
+        .select(user_disciplines::discipline)
+        .load::<Discipline>(conn)
+        .await
+}
+
 pub async fn get_profile(
     Path(id): Path<Uuid>,
     State(pool): State<Pool>,
 ) -> Result<Json<ProfileDetailsResponse>, (StatusCode, String)> {
     let mut conn = pool.get().await.map_err(internal_error)?;
 
-    let profile = profiles::table
-        .filter(profiles::id.eq(id))
-        .first::<Profile>(&mut conn)
+    let profile = fetch_profile_by_id(&mut conn, id)
         .await
         .map_err(|err| match err {
             diesel::result::Error::NotFound => (StatusCode::NOT_FOUND, "Profile not found".into()),
             _ => internal_error(err),
         })?;
 
-    let user_disciplines = profiles::table
-        .inner_join(user_disciplines::table)
-        .filter(profiles::id.eq(id))
-        .select(user_disciplines::discipline)
-        .load::<Discipline>(&mut conn)
+    let user_disciplines = fetch_user_disciplines_by_profile_id(&mut conn, id)
         .await
         .map_err(internal_error)?;
 
-    Ok(Json(ProfileDetailsResponse {
-        id: profile.id,
-        username: profile.username,
-        first_name: profile.first_name,
-        last_name: profile.last_name,
-        disciplines: user_disciplines,
-    }))
+    Ok(Json((profile, user_disciplines).into()))
 }
